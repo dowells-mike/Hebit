589177aa1eb83d2d2b4ce8b05d07e8806de36d7a
dowells-mike <mikedowells400@gmail.com>
2025-03-09 01:24:20 +0000

    feat(mobile): create Hilt application class

--- mobile/app/src/main/java/com/hebit/app/HebitApplication.kt
+++ mobile/app/src/main/java/com/hebit/app/HebitApplication.kt
@@ -3,5 +3,9 @@ package com.hebit.app
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+/**
+ * Main application class for Hebit
+ * HiltAndroidApp annotation triggers Hilt's code generation
+ */
 @HiltAndroidApp
 class HebitApplication : Application()
